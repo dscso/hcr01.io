120288edcc17f2c2b8a0b2b4a3608f3c31fc8c20
Jurek Weber <jurek@bessermailer.de>
2016-01-08 16:03:25 +0100

    structured code

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,49 +1,51 @@
 var app = angular.module('app', [
-  'ngRoute',
-  'ngSanitize'
+	'ngRoute',
+	'ngSanitize'
 ]);
 
 app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/md/:url', {
-        templateUrl: 'templates/md.html',
-        controller: 'templateCtrl'
-      }).when('/', {
-        templateUrl: 'templates/home.html',
-      }).otherwise({
-        redirectTo: "/"
-      });
-}]);
+	function($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/md/:url', {
+				templateUrl: 'templates/md.html',
+				controller: 'templateCtrl'
+			}).when('/', {
+				templateUrl: 'templates/home.html',
+			}).otherwise({
+				redirectTo: "/"
+			});
+		}
+]);
 
 app.factory('routeNavigation', function($location) {
-  var routes = settings.routes;
-  return {
-    routes: routes,
-    activeRoute: function (route) {
-      return route.path === $location.path();
-    }
-  };
+	var routes = settings.routes;
+	return {
+		routes: routes,
+		activeRoute: function (route) {
+			return route.path === $location.path();
+		}
+	};
 });
 
 app.directive('navigation', function (routeNavigation) {
-    return {
-        restrict: "E",
-        templateUrl: "templates/navigation.html",
-        controllerAs: "navCtrl",
-        controller: function ($scope) {
-           $scope.routes = routeNavigation.routes;
-           $scope.activeRoute = routeNavigation.activeRoute;
-        }
-    };
+	return {
+		restrict: "E",
+		templateUrl: "templates/navigation.html",
+		controllerAs: "navCtrl",
+		controller: function ($scope) {
+			$scope.routes = routeNavigation.routes;
+			$scope.activeRoute = routeNavigation.activeRoute;
+		}
+	};
 });
 
 app.controller('templateCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-    $http.get('content/md/' + $routeParams.url + '.md').success(function(data) {
-      $scope.html = markdown.toHTML(data);
-    }).error(function (err) {
-      $scope.html = "<h2>404 - Not found</h2>";
-    });
-    $scope.url = $routeParams.url;
-}]);
+	function($scope, $routeParams, $http) {
+		$http.get('content/md/' + $routeParams.url + '.md').success(function(data) {
+			$scope.html = markdown.toHTML(data);
+		}).error(function (err) {
+			$scope.html = "<h2>404 - Not found</h2>";
+		});
+		$scope.url = $routeParams.url;
+	}
+]);
--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,20 +1,20 @@
 var settings = {
-  "routes" : [
-    {
-      "name":"Home",
-      "path":"/"
-    },
-    {
-      "name":"Diagramms",
-      "path":"/md/diagramms"
-    },
-    {
-      "name":"Documentary",
-      "path":"/md/doc"
-    },
-    {
-      "name":"About",
-      "path":"/md/about"
-    },
-  ]
+	"routes" : [
+		{
+			"name":"Home",
+			"path":"/"
+		},
+		{
+			"name":"Diagramms",
+			"path":"/md/diagramms"
+		},
+		{
+			"name":"Documentary",
+			"path":"/md/doc"
+		},
+		{
+			"name":"About",
+			"path":"/md/about"
+		},
+	]
 }
